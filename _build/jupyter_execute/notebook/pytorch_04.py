76d03bcfe60aa28b04388278c7382364db4bae51
yamada-kd <yamada@tohoku.ac.jp>
2024-04-11 22:26:18 +0900

    edit

--- _build/jupyter_execute/notebook/pytorch_04.py
+++ _build/jupyter_execute/notebook/pytorch_04.py
@@ -92,20 +92,20 @@ if __name__ == "__main__":
 
 # ### 文字エンコード
 
-# 人工知能に何かの情報を処理させる際には，機械がそれを理解できるように何らかの方法でデータを数字に変換しなければなりませんが，これをエンコードと言います．例えば，自然言語に含まれる単語を，処理したいデータセット中の登場順に数字に変換する，みたいなことです．機械に処理させるためには自然言語では都合が悪く，数字データが都合が良いからです．そのような単語を数字に割り振って人工知能に入力しても，もちろん計算自体は可能です．しかし，この整数エンコーディングにはふたつの欠点があるそうです．あるそうです，と書きましたが，これは TensorFlow の公式ウェブサイトにそう書いてあったことを抜き出したためです．
+# 人工知能に何かの情報を処理させる際には，機械がそれを理解できるように何らかの方法でデータを数字に変換しなければなりませんが，これをエンコードと言います．例えば，自然言語に含まれる単語を，処理したいデータセット中の登場順に数字に変換する，みたいなことです．機械に処理させるためには自然言語では都合が悪く，数字データが都合が良いからです．そのような単語を数字に割り振って人工知能に入力しても，もちろん計算自体は可能です．しかし，この整数エンコーディングにはふたつの欠点があると，TensorFlow の公式ウェブサイトに書いてありました．
 # 
 # 
 # *   整数エンコーディングは単語間のいかなる関係性を含まない．
 # *   整数エンコーディングは人工知能にとっては解釈が難しい．例えば，線形分類器はそれぞれの特徴量について単一の重みしか学習しないため，ふたつの単語が似ていることとそれらのエンコーディングが似ていることの間には何の関係もなく，特徴と重みの組み合わせに意味がない．
 # 
-# 最初のひとつは学習全体を通して考えると人工知能のパラメータの方で解決できるし，ふたつ目も非線形な関数を近似できる人工知能を使えば解決できるため，回避できない問題とは思えませんが．
+# ただし，最初のひとつは学習全体を通して考えると人工知能のパラメータの方で担えば解決できるし，ふたつ目も非線形な関数を近似できる人工知能を使えば解決できるため，回避できない問題とは思えません．
 # 
-# これとは別のエンコード方法として，ワンホットエンコーディングがあります．ワンホットエンコーディングはワンホットベクトルに単語を対応させます．ワンホットベクトルとは，ベクトルのある要素が 1 で他の全要素が 0 であるベクトルです．以下のような感じです．
+# これとは別のエンコード方法として，ワンホットエンコーディングがあります．ワンホットエンコーディングはワンホットベクトルに単語を対応させます．ワンホットベクトルとは，ベクトルのある要素が1で他の全要素が0であるベクトルです．以下のような感じです．
 # 
 # <img src="https://github.com/yamada-kd/introduction-to-artificial-intelligence/blob/main/image/oneHotEmbedding.svg?raw=1" width="100%" />
 # 
 # 
-# この方法は非効率的です．ワンホットベクトルはとても疎です．ボキャブラリに 10000 個の単語がある場合，各ベクトルはその要素の 99.99% が 0 からなります．学習を効率よく進めるためには多くの重みパラメータを設定しなければならないことになるでしょう．また，入力ベクトルが場合によってはものすごく長くなるでしょう．
+# この方法は非効率的です．ワンホットベクトルはとても疎です．ボキャブラリに10000個の単語がある場合，各ベクトルはその要素の99.99%が0からなります．学習を効率よく進めるためには多くの重みパラメータを設定しなければならないことになるでしょう．また，入力ベクトルが場合によってはものすごく長くなるでしょう．
 # 
 # 現在のところ最も良い方法と考えられるのは単語埋め込み（word embedding）です．単語埋め込みを利用すると似たような単語が似たようなベクトルへとエンコードされます（学習の過程で）．埋め込みは浮動小数点数で行い，密なベクトルができます．以下のような感じです．
 # 
@@ -114,9 +114,9 @@ if __name__ == "__main__":
 
 # ### 入出力データ形式
 
-# 次の節では，実際の LSTM の実装方法を紹介します．RNN を TensorFlow（や PyTorch）で利用する場合には，その入力データと出力データで少しずつ書き方が変わってしまい，これをしっかり書かないとエラーが起こってしまいます．また，RNN は入力ベクトルの最初から順々にデータを処理する単方向でのデータ処理と，最後から最初に遡りながらデータを処理する逆方向の処理を順方向の処理と同時に行う双方向のデータ処理があり，これを利用するかどうかでも出力が変化します．
+# 次の節では，実際の LSTM の実装方法を紹介します．RNN を PyTorch（や TensorFlow）で利用する場合には，その入力データと出力データで少しずつ書き方が変わってしまい，これをしっかり書かないとエラーが起こってしまいます．また，RNN は入力ベクトルの最初から順々にデータを処理する単方向でのデータ処理と，最後から最初に遡りながらデータを処理する逆方向の処理を順方向の処理と同時に行う双方向のデータ処理があり，これを利用するかどうかでも出力が変化します．
 # 
-# RNN を活用したい問題としては 0 か 1 という分類をする問題があると思います．また，これとは異なり，配列を入力に配列を出力させたいときもあるはずです．さらに，分類ではなく回帰をしたい場合もあるはずです．単語（整数）の多次元のベクトルへの埋め込み（embedding）が必要な場合もあります．例えば `1` という文字は人工知能に入力するためには `[0.2, 0.1, 0.3, 0.4]` みたいなベクトルに変換しなければなりません．何次元のベクトルでも良いです．また，`[1, 0, 0, 0]` みたいなワンホットエンコーディングと呼ばれる埋め込み方法でも良いです．これらの種類をまとめると以下の表のようになります．実装方法がそれぞれにおいてほんの少しずつ異なります．ちょっと厄介ですね．
+# RNN を活用したい問題としては0か1という分類をする問題があると思います．また，これとは異なり，配列を入力に配列を出力させたいときもあるはずです．さらに，分類ではなく回帰をしたい場合もあるはずです．単語（整数）の多次元のベクトルへの埋め込み（embedding）が必要な場合もあります．例えば `1` という文字は人工知能に入力するためには `[0.2, 0.1, 0.3, 0.4]` みたいなベクトルに変換しなければなりません．何次元のベクトルでも良いです．また，`[1, 0, 0, 0]` みたいなワンホットエンコーディングと呼ばれる埋め込み方法でも良いです．これらの種類をまとめると以下の表のようになります．実装方法がそれぞれにおいてほんの少しずつ異なります．ちょっと厄介ですね．
 # 
 # 入力 | 出力 | 問題 | 埋め込み | 向き
 # :---: | :---: | :---: | :---: | :---:
@@ -137,11 +137,11 @@ if __name__ == "__main__":
 # 配列 | 配列 | 回帰 | 必要 | 単方向
 # <font color="Crimson">配列</font> | <font color="Crimson">配列</font> | <font color="Crimson">回帰</font> | <font color="Crimson">必要</font>  | <font color="Crimson">双方向</font>
 # 
-# ここでは，「単方向で入力ベクトルが配列で出力ベクトルがスカラ（1 要素からなるベクトル）で問題が分類で文字の埋め込みが不要なパターン」と「双方向で入力ベクトルと出力ベクトルがともに配列で問題が回帰で埋め込みが必要なパターン」のふたつの実装を紹介します．
+# ここでは，「単方向で入力ベクトルが配列で出力ベクトルがスカラ（1要素からなるベクトル）で問題が分類で文字の埋め込みが不要なパターン」と「双方向で入力ベクトルと出力ベクトルがともに配列で問題が回帰で埋め込みが必要なパターン」のふたつの実装を紹介します．
 # 
 
 # ```{note}
-# 全部紹介しても良いのですけど紙面というかコラボ面があれですからね…
+# 全部紹介しても良いのですけど紙面の都合上避けます．
 # ```
 
 # ## RNN の実装
@@ -185,7 +185,7 @@ if __name__ == "__main__":
 # この出力の値 $\mathbf{h}$ は $\mathbf{s}$ と同様に保存され，次の時間（timestep）での計算に用いられます．このように，LSTM は以前の情報（ひとつ前の timestep の情報）を使って出力をする人工知能です．ひとつ前の情報にはさらにひとつ前の情報が記憶されており，またそのひとつ前の情報にはさらにひとつ前の情報が記憶されています．よって，LSTM ではどんな長い配列情報であっても（パラメータサイズが十分なら），すべての文脈に関わる情報を記憶することが可能です．
 
 # ```{note}
-# 決して MLP が配列情報を扱えないとは書いていません．
+# 決して MLP が配列情報を扱えないというわけではありません．
 # ```
 
 # ### 実装方法①
@@ -204,79 +204,83 @@ if __name__ == "__main__":
 
 
 #!/usr/bin/env python3
-import tensorflow as tf
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras import Model
-tf.random.set_seed(0)
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
+from torch.utils.data import DataLoader, TensorDataset
+torch.manual_seed(0)
 
 def main():
-    # データセットの生成
-    tx=[[[1.1,0.1],[2.2,0.3],[3.0,0.3],[4.0,1.0]],[[2.0,0.9],[0.1,0.8],[3.0,0.7],[4.0,0.1],[1.0,0.3]],[[2.0,1.0],[3.0,0.6],[4.0,0.6]]]
-    tx=tf.keras.preprocessing.sequence.pad_sequences(tx,padding="post",dtype=np.float32)
-    tt=[1,2,0]
-    tt=tf.convert_to_tensor(tt)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # データセットの生成．
+    tx = [
+        torch.tensor([[1.1, 0.1], [2.2, 0.3], [3.0, 0.3], [4.0, 1.0]]),
+        torch.tensor([[2.0, 0.9], [0.1, 0.8], [3.0, 0.7], [4.0, 0.1], [1.0, 0.3]]),
+        torch.tensor([[2.0, 1.0], [3.0, 0.6], [4.0, 0.6]])
+    ]
+    tx = torch.nn.utils.rnn.pad_sequence(tx, batch_first=True, padding_value=0.0).to(device)
+    tt = torch.tensor([1, 2, 0], dtype=torch.long).to(device)
 
     # ネットワークの定義
-    model=Network()
-    cce=tf.keras.losses.SparseCategoricalCrossentropy()
-    acc=tf.keras.metrics.SparseCategoricalAccuracy()
-    optimizer=tf.keras.optimizers.Adam()
+    model = Network().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
 
-    # 学習1回の記述
-    @tf.function
-    def inference(tx,tt):
-        with tf.GradientTape() as tape:
-            ty=model.call(tx)
-            costvalue=cce(tt,ty) #正解と出力の順番はこの通りにする必要がある．
-        gradient=tape.gradient(costvalue,model.trainable_variables)
-        optimizer.apply_gradients(zip(gradient,model.trainable_variables))
-        accvalue=acc(tt,ty)
-        return costvalue,accvalue
+    dataset = TensorDataset(tx, tt)
+    loader = DataLoader(dataset, batch_size=3) # 全データを一括で読み込みたいためミニバッチサイズは3に設定．
 
-    # 学習前の人工知能がどのような出力をしているのかを確認
-    ty=model.call(tx)
-    print("Output vector:",ty)
-    print("Target vector:",tt)
+    # 学習前の人工知能がどのような出力をしているのかを確認．
+    with torch.no_grad():
+        ty = model(tx)
+        print("Output vector:", ty)
+        print("Target vector:", tt)
 
-    # 学習ループ
-    for epoch in range(1,1000+1):
-        traincost,trainacc=inference(tx,tt)
-        if epoch%50==0:
-            print("Epoch {:5d}: Training cost= {:.4f}, Training ACC= {:.4f}".format(epoch,traincost,trainacc))
+    # 学習ループ．
+    for epoch in range(1, 1001):
+        for data, target in loader:
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+        if epoch % 50 == 0:
+            print(f'Epoch {epoch}: Training Loss= {loss.item()}')
 
-    # 学習後の人工知能がどのような出力をしているのかを確認
-    ty=model.call(tx)
-    print("Output vector:",ty)
-    print("Target vector:",tt)
+    # 学習後の人工知能がどのような出力をしているのかを確認．
+    with torch.no_grad():
+        ty = model(tx)
+        print("Output vector:", ty)
+        print("Target vector:", tt)
 
-    # MLPの場合と比較しつつ，ハイパーパラメータを変更しながらどのような挙動をしているか確認．
-
-class Network(Model):
+class Network(nn.Module):
     def __init__(self):
-        super(Network,self).__init__()
-        self.lstm=LSTM(50)
-        self.fc=Dense(3,activation="softmax")
+        super(Network, self).__init__()
+        self.lstm = nn.LSTM(input_size=2, hidden_size=50, batch_first=True)
+        self.fc = nn.Linear(50, 3)
 
-    def call(self,tx):
-        ty=self.lstm(tx)
-        ty=self.fc(ty)
+    def forward(self, tx):
+        lstm_out, _ = self.lstm(tx)
+        ty = self.fc(lstm_out[:, -1, :])  # 最後のタイムステップのみを使用．
         return ty
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
 
 # 以下の部分はデータセットを生成するための記述です．`tx` が入力ベクトルを格納するための変数です．入力ベクトルは二次元配列なので，`tx` は三次元配列の構造をとります．
 # ```python
-#     # データセットの生成
-#     tx=[[[1.1,0.1],[2.2,0.3],[3.0,0.3],[4.0,1.0]],[[2.0,0.9],[0.1,0.8],[3.0,0.7],[4.0,0.1],[1.0,0.3]],[[2.0,1.0],[3.0,0.6],[4.0,0.6]]]
-#     tx=tf.keras.preprocessing.sequence.pad_sequences(tx,padding="post",dtype=np.float32)
-#     tt=[1,2,0]
-#     tt=tf.convert_to_tensor(tt)
+#     # データセットの生成．
+#     tx = [
+#         torch.tensor([[1.1, 0.1], [2.2, 0.3], [3.0, 0.3], [4.0, 1.0]]),
+#         torch.tensor([[2.0, 0.9], [0.1, 0.8], [3.0, 0.7], [4.0, 0.1], [1.0, 0.3]]),
+#         torch.tensor([[2.0, 1.0], [3.0, 0.6], [4.0, 0.6]])
+#     ]
+#     tx = pad_sequence(tx, batch_first=True, padding_value=0.0).to(device)
+#     tt = torch.tensor([1, 2, 0], dtype=torch.long).to(device)
 # ```
-# `tf.keras.preprocessing.sequence.pad_sequences()` はゼロパディングのための記述です．これによって以下のようにデータは変換されます．
+# `tx = torch.nn.utils.rnn.pad_sequence(tx, batch_first=True, padding_value=0.0).to(device)` はゼロパディングのための記述です．これによって以下のようにデータは変換されます．
 # 
 # 入力ベクトル | ターゲットベクトル
 # :--- | :---:
@@ -284,18 +288,18 @@ if __name__=="__main__":
 # [ [2.0 ,0.9], [0.1, 0.8], [3.0, 0.7], [4.0, 0.1], [1.0, 0.3] ] | [ 2 ]
 # [ [2.0, 1.0], [3.0, 0.6], [4.0, 0.6], [0.0, 0.0], [0.0, 0.0] ] | [ 0 ]
 
-# その他の部分は MLP のときと変わりません．ネットワークの定義は異なっており，以下ように行います．MLP の場合は `Dense()` だけを用いました．これに対して LSTM を実装したい場合は `LSTM()` を用います．`LSTM(50)` となっていますが，これは LSTM のユニットサイズが50ということです．また，`Dense(3,activation="softmax")` とありますが，これは全結合層（最も基本的な層）でニューロンのサイズは3個であることを意味しています．また，この層は出力層です．前述の MNIST を扱った MLP が入力層784，中間層500，出力層10のサイズでしたが，これを `(784, 500, 10)`と表現します．これに対して，このネットワークの1 timestep では `(2, 50, 3)` という MLP の計算がされます．これが LSTM の仕組みによって，timestep 分（5回）繰り返されます．この場合，出力は配列でないため，最後の `(2, 50, 3)` の計算の出力のみが最終的な予測に用いられます．
+# その他の部分は MLP のときと変わりません．ネットワークの定義は異なっており，以下ように行います．MLP の場合は `nn.Linear()` だけを用いました．これに対して LSTM を実装したい場合は `nn.LSTM()` を用います．ここでは，LSTM のユニットサイズを50にしています．また，`nn.Linear(50, 3)` とありますが，これは全結合層（最も基本的な層）でニューロンのサイズは3個であることを意味しています．また，この層は出力層です．以前の章の MNIST を扱った MLP が入力層784，中間層500，出力層10のサイズでしたが，これを `(784, 500, 10)`と表現するとします．これに対して，このネットワークの1 timestep では `(2, 50, 3)` という MLP の計算がされます．この `2` というのは各タイムステップのベクトルの要素数です．最後の `3` というのはターゲットベクトルの要素数です．これが LSTM の仕組みによって，timestep 分（5回）繰り返されます．この場合，出力は配列でないため，最後の `(2, 50, 3)` の計算の出力のみを最終的な予測に用います．
 # 
 # ```python
-# class Network(Model):
+# class Network(nn.Module):
 #     def __init__(self):
-#         super(Network,self).__init__()
-#         self.lstm=LSTM(50)
-#         self.fc=Dense(3,activation="softmax")
-#     
-#     def call(self,tx):
-#         ty=self.lstm(tx)
-#         ty=self.fc(ty)
+#         super(Network, self).__init__()
+#         self.lstm = nn.LSTM(input_size=2, hidden_size=50, batch_first=True)
+#         self.fc = nn.Linear(50, 3)
+# 
+#     def forward(self, tx):
+#         lstm_out, _ = self.lstm(tx)
+#         ty = self.fc(lstm_out[:, -1, :])  # 最後のタイムステップのみを使用．
 #         return ty
 # ```
 
@@ -317,109 +321,88 @@ if __name__=="__main__":
 
 
 #!/usr/bin/env python3
-import tensorflow as tf
-from tensorflow.keras.layers import Embedding
-from tensorflow.keras.layers import Bidirectional
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras import Model
-tf.random.set_seed(0)
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
+torch.manual_seed(0)
 
 def main():
-    # データセットの生成
-    tx=[[7,5,8],[3,9,3,4,6],[2,3,4,1]]
-    tt=[[[6.2,1.1],[3.5,2.1],[2.0,1.1]],[[4.5,3.8],[4.1,4.9],[3.4,4.6],[2.7,1.7],[2.1,2.5]],[[1.2,1.0],[4.4,3.3],[3.1,2.8],[2.7,1.6]]]
-    tx=tf.keras.preprocessing.sequence.pad_sequences(tx,padding="post",dtype=np.int32,value=0)
-    tt=tf.keras.preprocessing.sequence.pad_sequences(tt,padding="post",dtype=np.float32,value=-1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # ネットワークの定義
-    model=Network()
-    mse=tf.keras.losses.MeanSquaredError()
-    optimizer=tf.keras.optimizers.Adam()
+    # データセットの生成．
+    tx = [[7, 5, 8], [3, 9, 3, 4, 6], [2, 3, 4, 1]]
+    tt = [[[6.2, 1.1], [3.5, 2.1], [2.0, 1.1]], [[4.5, 3.8], [4.1, 4.9], [3.4, 4.6], [2.7, 1.7], [2.1, 2.5]], [[1.2, 1.0], [4.4, 3.3], [3.1, 2.8], [2.7, 1.6]]]
 
-    # 学習1回の記述
-    @tf.function
-    def inference(tx,tt):
-        with tf.GradientTape() as tape:
-            ty=model.call(tx)
-            costvalue=mse(tt,ty)
-        gradient=tape.gradient(costvalue,model.trainable_variables)
-        optimizer.apply_gradients(zip(gradient,model.trainable_variables))
-        return costvalue
+    tx = torch.nn.utils.rnn.pad_sequence([torch.tensor(x) for x in tx], batch_first=True, padding_value=0).to(device)
+    tt = torch.nn.utils.rnn.pad_sequence([torch.tensor(x) for x in tt], batch_first=True, padding_value=-1).to(device)
 
-    # 学習前の人工知能がどのような出力をしているのかを確認
-    ty=model.call(tx)
-    print("Output vector:",ty)
-    print("Target vector:",tt)
+    model = Network().to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters())
 
-    # 学習ループ
-    for epoch in range(1,1000+1):
-        traincost=inference(tx,tt)
-        if epoch%50==0:
-            print("Epoch={0:5d} Cost={1:7.4f}".format(epoch,float(traincost)))
+    # 学習前の出力を確認．
+    with torch.no_grad():
+        ty = model(tx)
+        print("Output vector:", ty)
+        print("Target vector:", tt)
 
-    # 学習後の人工知能がどのような出力をしているのかを確認
-    ty=model.call(tx)
-    print("Output vector:",ty)
-    print("Target vector:",tt)
+    # 学習ループ．
+    for epoch in range(1, 1001):
+        optimizer.zero_grad()
+        output = model(tx)
+        loss = criterion(output, tt)
+        loss.backward()
+        optimizer.step()
 
-    # 上の例と比較しながら挙動を確認．
+        if epoch % 50 == 0:
+            print("Epoch={:5d} Cost={:7.4f}".format(epoch, loss.item()))
 
-class Network(Model):
+    # 学習後の出力を確認．
+    with torch.no_grad():
+        ty = model(tx)
+        print("Output vector:", ty)
+        print("Target vector:", tt)
+
+class Network(nn.Module):
     def __init__(self):
-        super(Network,self).__init__()
-        self.embed=Embedding(input_dim=9+1,output_dim=3,mask_zero=True)
-        self.lstm=Bidirectional(LSTM(units=50,return_sequences=True))
-        self.fc=Dense(units=2)
+        super(Network, self).__init__()
+        self.embed = nn.Embedding(num_embeddings=10, embedding_dim=3, padding_idx=0)
+        self.lstm = nn.LSTM(input_size=3, hidden_size=50, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(in_features=100, out_features=2)
 
-    def call(self,tx):
-        ty=self.embed(tx)
-        ty=self.lstm(ty)
-        ty=self.fc(ty)
+    def forward(self, tx):
+        ty = self.embed(tx)
+        ty, _ = self.lstm(ty)
+        ty = self.fc(ty)
         return ty
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
 
-# このプログラムでは以下のような関数をインポートしています．この `Embedding()` は `7` とか `5` とか `8` というようなダミー変数をそれぞれ，`[5.1234, 0.4516, 1.4631]` とか `[1.5462, 0.4641, 0.9798]` とか `[3.7486, 0.7672, 4.423]` みたいなベクトルに変換するための関数です．ダミー変数を何らかのベクトル空間に埋め込むという作業をします．
-# ```pyton
-# from tensorflow.keras.layers import Embedding
-# ```
+# このプログラムではネットワークを定義するクラス内で `nn.Embedding()` を利用しています．これは `7` とか `5` とか `8` というようなダミー変数をそれぞれ，`[5.1234, 0.4516, 1.4631]` とか `[1.5462, 0.4641, 0.9798]` とか `[3.7486, 0.7672, 4.423]` みたいなベクトルに変換するための関数です．ダミー変数を何らかのベクトル空間に埋め込むという作業をします．
+# 
 
 # 次に，以下の部分ですが，ダミー変数の場合は0でパディングを行いました．それに対してこのターゲットベクトルに関しては，`-1` というターゲットベクトルに出現しそうにない値でパディングを行っています．
 # ```python
-# tt=tf.keras.preprocessing.sequence.pad_sequences(tt,padding="post",dtype=np.float32,value=-1)
+# tt = torch.nn.utils.rnn.pad_sequence([torch.tensor(x) for x in tt], batch_first=True, padding_value=-1).to(device)
 # ```
 
-# 学習1回分の記述がこれまでと少し異なっています．回帰問題においては正確度は計算できないため，その部分を削除しました．
-# ```python
-#     # 学習1回の記述
-#     @tf.function
-#     def inference(tx,tt):
-#         with tf.GradientTape() as tape:
-#             ty=model.call(tx)
-#             costvalue=mse(tt,ty)
-#         gradient=tape.gradient(costvalue,model.trainable_variables)
-#         optimizer.apply_gradients(zip(gradient,model.trainable_variables))
-#         return costvalue
-# ```
-
-# 実際のネットワークは以下で定義しています．`Embedding(input_dim=9+1,output_dim=3,mask_zero=True)` における `9+1` は入力ベクトルのダミー変数の種類が `1` から `9` の9種類あることに加えて，配列をパディングするために `0` を利用するためです．この `0` から `9` の10種類の値を3要素からなるベクトルデータに変換する作業をこれで行います．`LSTM(units=50,return_sequences=True)` の部分は前述の例と異なります．`return_sequences=True` というオプションが指定されていますが，これは LSTM の最終的な出力としてひとつの固定長のベクトルを返すのではなくて，入力ベクトルの長さに合った個数のベクトルを返すためのオプションです．入力ベクトルはパディングされて5文脈のベクトルに変換されているので，この LSTM では5文脈のベクトルが返されます．また，`Bidirectional()` は RNN を双方向で計算させるためのものです．LSTM 自体は50個のニューロンで定義されていますが，双方向なので正方向の出力と負方向の出力が連結された100の大きさのベクトルが返ります．最後の `Dense()` は全結合層であり，出力データの各時刻における値が2要素からなるベクトルなのでニューロンは2個に設定します．プログラムを実行すると学習済みの人工知能の出力は教師ベクトルと類似していることが確認できます．
+# 実際のネットワークは以下で定義しています．`nn.Embedding(num_embeddings=9+1, embedding_dim=3, padding_idx=0)` における `9+1` は入力ベクトルのダミー変数の種類が `1` から `9` の9種類あることに加えて，配列をパディングするために `0` を利用するためです．この `0` から `9` の10種類の値を3要素からなるベクトルデータに変換する作業をこれで行います．`nn.LSTM(input_size=3, hidden_size=50, batch_first=True, bidirectional=True)` の部分は前述の例と異なります．`bidirectional=True` は RNN を双方向で計算させるためのものです．LSTM 自体は50個のニューロンで定義されていますが，双方向なので正方向の出力と負方向の出力が連結された100の大きさのベクトルが返ります．最後の `nn.Linear()` は全結合層であり，出力データの各時刻における値が2要素からなるベクトルなのでニューロンは2個に設定します．プログラムを実行すると学習済みの人工知能の出力は教師ベクトルと類似していることが確認できます．
 # 
 # ```python
-# class Network(Model):
+# class Network(nn.Module):
 #     def __init__(self):
-#         super(Network,self).__init__()
-#         self.embed=Embedding(input_dim=9+1,output_dim=3,mask_zero=True)
-#         self.lstm=Bidirectional(LSTM(units=50,return_sequences=True))
-#         self.fc=Dense(units=2)
-#     
-#     def call(self,tx):
-#         ty=self.embed(tx)
-#         ty=self.lstm(ty)
-#         ty=self.fc(ty)
+#         super(Network, self).__init__()
+#         self.embed = nn.Embedding(num_embeddings=9+1, embedding_dim=3, padding_idx=0)
+#         self.lstm = nn.LSTM(input_size=3, hidden_size=50, batch_first=True, bidirectional=True)
+#         self.fc = nn.Linear(in_features=100, out_features=2)
+# 
+#     def forward(self, tx):
+#         ty = self.embed(tx)
+#         ty, _ = self.lstm(ty)
+#         ty = self.fc(ty)
 #         return ty
 # ```
 
